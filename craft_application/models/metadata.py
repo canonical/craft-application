b23730a1e5e6bb25cd5104e6f68d689b4852baba
Alex Lowe <alex.lowe@canonical.com>
2023-06-07 13:54:00 -0400

    util: error formatting

--- craft_application/models/metadata.py
+++ craft_application/models/metadata.py
@@ -3,18 +3,19 @@
 # Copyright 2023 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
-# by the Free Software Foundation.
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
 # SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
+# You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Base project metadata model."""
 import pydantic
+from typing_extensions import override
 
 from craft_application.models.base import CraftBaseConfig, CraftBaseModel
 
@@ -26,6 +27,7 @@ class BaseMetadata(CraftBaseModel):
     the application's output.
     """
 
+    @override
     class Config(CraftBaseConfig):
         """Allows writing of unknown fields."""
 
