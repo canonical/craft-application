c3b9c60d89b2ce3f8f21b121d232f41df7576361
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2023-09-06 21:17:36 -0300

    update other commands module docstring
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_application/commands/other.py
+++ craft_application/commands/other.py
@@ -11,7 +11,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Version command."""
+"""Miscellaneous commands in the 'Other' command group."""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
