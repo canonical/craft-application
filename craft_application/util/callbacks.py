5f686ede1576c880bf0a08f28fcbd6e0316eda4c
Alex Lowe <alex.lowe@canonical.com>
2023-11-21 11:28:23 -0500

    style: typing for callbacks util

--- craft_application/util/callbacks.py
+++ craft_application/util/callbacks.py
@@ -16,20 +16,44 @@
 """Utilities related to callbacks."""
 from __future__ import annotations
 
-from typing import Callable, Iterable
+from typing import TYPE_CHECKING, Callable, Iterable, Literal, overload
+
+if TYPE_CHECKING:  # pragma: no-cover
+    # Caution: Removing these from type checking will result in circular imports.
+    from craft_application.commands.base import ParserCallback, RunCallback
 
 
-def get_unique_callbacks(cls: type, callback_name: str) -> Iterable[Callable]:
+@overload
+def get_unique_callbacks(
+    cls: type, callback_name: Literal["_parse_callback"]
+) -> Iterable[ParserCallback]:
+    ...
+
+
+@overload
+def get_unique_callbacks(
+    cls: type, callback_name: Literal["_prologue", "_epilogue"]
+) -> Iterable[RunCallback]:
+    ...
+
+
+def get_unique_callbacks(  # pyright: ignore[reportUnknownParameterType]
+    cls: type, callback_name: str
+) -> Iterable[Callable]:  # type: ignore[type-arg]
     """Get all unique callbacks in a class's inheritance tree.
 
     Guarantees order to be the reverse of the method resolution order (that is,
     starting with ``object`` and working its way down the resolution tree to the
     class itself). This means that the callbacks for child classes may depend on
     or modify the results of the callbacks for the parent class.
+
+    :param cls:  The class to search for callbacks
+    :param callback_name:  The name of the function
+    :return: Ordered callbacks starting at the top of the class tree.
     """
-    callbacks = []
+    callbacks = []  # pyright: ignore[reportUnknownVariableType]
     for class_ in reversed(cls.mro()):
         callback = getattr(class_, callback_name, None)
         if callback is not None and callback not in callbacks:
-            callbacks.append(callback)
-    return callbacks
+            callbacks.append(callback)  # pyright: ignore[reportUnknownMemberType]
+    return callbacks  # pyright: ignore[reportUnknownVariableType]
