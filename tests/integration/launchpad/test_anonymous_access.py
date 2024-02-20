05f1fba27e230047b590fa314a3bb13545f0838f
Alex Lowe <alex.lowe@canonical.com>
2024-02-20 18:44:13 -0500

    fix: update rendering of git repository paths
    Also adds integration tests

--- tests/integration/launchpad/test_anonymous_access.py
+++ tests/integration/launchpad/test_anonymous_access.py
@@ -1,16 +1,45 @@
 """Tests for anonymous access."""
 
-from craft_application.launchpad import Launchpad, models
+import pytest
+from craft_application.launchpad import models
 
 
 def test_get_basic_items(anonymous_lp):
-    lp = Launchpad("lp-test", anonymous_lp)
 
-    snapstore_server = lp.get_project("snapstore-server")
+    snapstore_server = anonymous_lp.get_project("snapstore-server")
     assert snapstore_server.name == "snapstore-server"
     assert snapstore_server.title == "Snap Store Server"
 
-    snap_recipes = list(models.SnapRecipe.find(lp, owner="lengau"))
+    snap_recipes = list(models.SnapRecipe.find(anonymous_lp, owner="lengau"))
     assert len(snap_recipes) > 0
     for recipe in snap_recipes:
         assert recipe.owner_name == "lengau"
+
+
+@pytest.mark.parametrize(
+    ("name", "path"),
+    [
+        ("python-apt", "~deity/python-apt/+git/python-apt"),
+        ("charmcraft", "~charmcraft-team/charmcraft/+git/charmcraft"),
+    ],
+)
+def test_get_real_repository_by_path(anonymous_lp, name, path):
+    repo = anonymous_lp.get_repository(path=path)
+
+    assert repo.git_https_url.endswith(name)
+    assert repo.name == name
+
+
+@pytest.mark.parametrize(
+    ("name", "owner", "project"),
+    [
+        ("python-apt", "deity", "python-apt"),
+        ("charmcraft", "charmcraft-team", "charmcraft"),
+        ("snapcraft", "sergiusens", "snapcraft"),
+    ],
+)
+def test_get_real_repository_by_name(anonymous_lp, name, owner, project):
+    repo = anonymous_lp.get_repository(name=name, owner=owner, project=project)
+
+    assert repo.name == name
+    assert repo.owner_name == owner
