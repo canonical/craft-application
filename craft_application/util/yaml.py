93bba904d157c08bcd688f2a7609e0e8cdec30fb
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-02-21 16:19:01 -0300

    linters: more black adjustments
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_application/util/yaml.py
+++ craft_application/util/yaml.py
@@ -107,8 +107,7 @@ def safe_yaml_load(stream: TextIO) -> Any:  # noqa: ANN401 - The YAML could be a
 @overload
 def dump_yaml(
     data: Any, stream: TextIO, **kwargs: Any  # noqa: ANN401 Any gets passed to pyyaml
-) -> None:
-    ...  # pragma: no cover
+) -> None: ...  # pragma: no cover
 
 
 @overload
@@ -116,8 +115,7 @@ def dump_yaml(
     data: Any,  # noqa: ANN401 Any gets passed to pyyaml
     stream: None = None,
     **kwargs: Any,  # noqa: ANN401 Any gets passed to pyyaml
-) -> str:
-    ...  # pragma: no cover
+) -> str: ...  # pragma: no cover
 
 
 def dump_yaml(data: Any, stream: TextIO | None = None, **kwargs: Any) -> str | None:
