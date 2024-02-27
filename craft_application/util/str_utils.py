f569021c1bbee40d20d7af5577609c0d22de1eae
Sheng Yu <sheng.yu@canonical.com>
2024-02-27 17:18:02 -0500

    feat: add strtobool function
    This allows convert a boolean string to a boolean.

---
+++ craft_application/util/str_utils.py
@@ -0,0 +1,29 @@
+#  This file is part of craft-application.
+#
+#  Copyright 2024 Canonical Ltd.
+#
+#  This program is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License version 3, as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+#  SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""String related functions."""
+
+
+def strtobool(value: str) -> bool:
+    """Convert a string to a boolean."""
+    if not isinstance(value, str):  # type: ignore[reportUnnecessaryIsInstance]
+        raise TypeError("Invalid str value: %r" % value)
+
+    value = value.strip().lower()
+    if value in {"true", "t", "yes", "y", "on", "1"}:
+        return True
+    if value in {"false", "f", "no", "n", "off", "0"}:
+        return False
+    raise ValueError(f"Invalid boolean value: {value}")
---
+++ tests/unit/util/test_str_utils.py
@@ -0,0 +1,79 @@
+# This file is part of craft-application.
+#
+# Copyright 2024 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Tests for internal str autilities."""
+
+import pytest
+from craft_application.util import str_utils
+
+
+@pytest.mark.parametrize(
+    ("data", "expected"),
+    [
+        ("true", True),
+        (" true", True),
+        ("True", True),
+        ("T", True),
+        ("t", True),
+        (" t ", True),
+        ("yes", True),
+        ("Yes", True),
+        ("Y", True),
+        ("y", True),
+        ("  y  ", True),
+        ("on", True),
+        ("On", True),
+        ("1", True),
+        (" 1 ", True),
+        ("false", False),
+        ("False", False),
+        ("  False", False),
+        ("F", False),
+        ("f", False),
+        ("no", False),
+        ("No", False),
+        ("N", False),
+        ("n", False),
+        ("  n", False),
+        ("off", False),
+        ("Off", False),
+        ("0", False),
+        ("0  ", False),
+    ],
+)
+def test_strtobool(data, expected):
+    actual = str_utils.strtobool(data)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ("data"),
+    [
+        (None),
+        ({}),
+        ([]),
+        (""),
+        (" "),
+        ("invalid"),
+        ("2"),
+        ("-"),
+        ("!"),
+        ("*"),
+    ],
+)
+def test_strtobool_error(data):
+    with pytest.raises((ValueError, TypeError), match="Invalid"):
+        str_utils.strtobool(data)
