156d584439bc2fd2bcd7b1b0989f96fa562a459c
Sheng Yu <sheng.yu@canonical.com>
2024-04-04 11:13:19 -0400

    feat(grammar): enable grammar-aware for all PartSpec
    This allows use `- on X to Y: values` in all parts.

--- craft_application/grammar.py
+++ craft_application/grammar.py
@@ -15,38 +15,61 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Grammar processor."""
 
-from typing import Any
+from typing import Any, cast
 
 from craft_grammar import GrammarProcessor  # type: ignore[import-untyped]
 
-_KEYS = [
-    "source",
+# Values that should return as a single object / list / dict.
+_NON_SCALAR_VALUES = [
+    "source-submodules",
+    "after",
+    "overlay-packages",
+    "build-attributes",
+    "permissions",
     "build-environment",
     "build-packages",
     "stage-packages",
     "build-snaps",
     "stage-snaps",
+    "overlay",
+    "organize",
+    "stage",
+    "prime",
 ]
 
-_SCALAR_VALUES = ["source"]
+# Values that should return a dict, not in a list.
+_DICT_ONLY_VALUES = [
+    "organize",
+]
 
 
 def process_part(
     *, part_yaml_data: dict[str, Any], processor: GrammarProcessor
 ) -> dict[str, Any]:
     """Process grammar for a given part."""
-    existing_keys = (key for key in _KEYS if key in part_yaml_data)
-
-    for key in existing_keys:
+    for key in part_yaml_data:
         unprocessed_grammar = part_yaml_data[key]
 
-        if key in _SCALAR_VALUES and isinstance(unprocessed_grammar, str):
+        # grammar aware models can only be a string or list of dict, skip any other type.
+        if isinstance(unprocessed_grammar, list):
+            if any(not isinstance(d, dict) for d in unprocessed_grammar):  # type: ignore[reportUnknownVariableType]
+                continue
+            if any(not isinstance(k, str) for d in unprocessed_grammar for k in d):  # type: ignore[reportUnknownVariableType]
+                continue
+            unprocessed_grammar = cast(list[dict[str, Any]], unprocessed_grammar)
+        elif isinstance(unprocessed_grammar, str):
             unprocessed_grammar = [unprocessed_grammar]
+        else:
+            continue
 
         processed_grammar = processor.process(grammar=unprocessed_grammar)
 
-        if key in _SCALAR_VALUES:
+        # special cases
+        # scalar values should return as a single object, not in a list.
+        # dict values should return as a dict, not in a list.
+        if key not in _NON_SCALAR_VALUES or key in _DICT_ONLY_VALUES:
             processed_grammar = processed_grammar[0] if processed_grammar else None
+
         part_yaml_data[key] = processed_grammar
 
     return part_yaml_data
