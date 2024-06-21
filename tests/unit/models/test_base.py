e10c9b89e4467fb70e7621f505a47cf535cf3478
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-06-21 10:13:04 -0300

    feat: add a way to set doc urls for models
    This is mainly used for validation errors: when a CraftBaseModel fails to be
    created due to Pydantic validation errors, the link to that specific model
    class is added to the error message shown to the user.

---
+++ tests/unit/models/test_base.py
@@ -0,0 +1,58 @@
+# This file is part of craft-application.
+#
+# Copyright 2023-2024 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Tests for CraftBaseModel"""
+from pathlib import Path
+
+import pydantic
+import pytest
+from craft_application import errors, models
+from overrides import override
+
+
+class MyBaseModel(models.CraftBaseModel):
+
+    value1: int
+    value2: str
+
+    @pydantic.validator("value1")
+    def _validate_value1(cls, _v):
+        raise ValueError("Bad value1 value")
+
+    @pydantic.validator("value2")
+    def _validate_value2(cls, _v):
+        raise ValueError("Bad value2 value")
+
+    @classmethod
+    @override
+    def model_reference_slug(cls) -> str | None:
+        return "/mymodel.html"
+
+
+def test_model_reference_slug_errors():
+    data = {
+        "value1": 1,
+        "value2": "hi",
+    }
+    with pytest.raises(errors.CraftValidationError) as err:
+        MyBaseModel.from_yaml_data(data, Path("testcraft.yaml"))
+
+    expected = (
+        "Bad testcraft.yaml content:\n"
+        "- bad value1 value (in field 'value1')\n"
+        "- bad value2 value (in field 'value2')"
+    )
+    assert str(err.value) == expected
+    assert err.value.doc_slug == "/mymodel.html"
