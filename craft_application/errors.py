8ce1f25a586e06b9622a13164a39e1d732a712df
Alex Lowe <alex.lowe@canonical.com>
2024-07-16 16:07:50 -0400

    style(lint): autoformat

--- craft_application/errors.py
+++ craft_application/errors.py
@@ -19,8 +19,8 @@ All errors inherit from craft_cli.CraftError.
 """
 from __future__ import annotations
 
-from collections.abc import Sequence
 import os
+from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 from craft_cli import CraftError
