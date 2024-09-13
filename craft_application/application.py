08f98cb450a3a995fa50e126a0064d336c97fd07
Sergio Schvezov <sergio.schvezov@canonical.com>
2024-09-13 08:46:03 -0300

    chore: factor run logic outside of try block
    This allows applications to override the inner run logic, with the end
    goal of capturing application specific exceptions and raise
    appropriate ones for Craft Application to handle.
    
    Without the logic split, overriding run to capture exceptions is
    virtually impossible as the run method holds a generic Exception
    handler.
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- craft_application/application.py
+++ craft_application/application.py
@@ -419,8 +419,9 @@ class Application:
         """Configure the application using any global arguments."""
 
     def _get_dispatcher(self) -> craft_cli.Dispatcher:
-        """Configure this application. Should be called by the run method.
+        """Configure this application.
 
+        Should be called by the _run_inner method.
         Side-effect: This method may exit the process.
 
         :returns: A ready-to-run Dispatcher object
@@ -562,68 +563,69 @@ class Application:
                     options=ValidatorOptions.ATTACHMENT | ValidatorOptions.SUPPORT
                 )
 
-    def run(  # noqa: PLR0912,PLR0915  (too many branches, too many statements)
-        self,
-    ) -> int:
+    def _run_inner(self) -> int:
+        """Actual run implementation."""
+        dispatcher = self._get_dispatcher()
+        command = cast(
+            commands.AppCommand,
+            dispatcher.load_command(self.app_config),
+        )
+        parsed_args = dispatcher.parsed_args()
+        platform = self.get_arg_or_config(parsed_args, "platform")
+        build_for = self.get_arg_or_config(parsed_args, "build_for")
+
+        # Some commands (e.g. remote build) can allow multiple platforms
+        # or build-fors, comma-separated. In these cases, we create the
+        # project using the first defined platform.
+        if platform and "," in platform:
+            platform = platform.split(",", maxsplit=1)[0]
+        if build_for and "," in build_for:
+            build_for = build_for.split(",", maxsplit=1)[0]
+
+        provider_name = command.provider_name(dispatcher.parsed_args())
+        managed_mode = command.run_managed(dispatcher.parsed_args())
+
+        # TODO: Move pro operations out to new service for managing Ubuntu Pro
+        # A ProServices instance will only be available for lifecycle commands,
+        # which may consume pro packages,
+        self._pro_services = getattr(dispatcher.parsed_args(), "pro", None)
+        # Check that pro services are correctly configured if available
+        self._check_pro_requirement(self._pro_services, managed_mode, self.is_managed())
+
+        craft_cli.emit.debug(f"Build plan: platform={platform}, build_for={build_for}")
+        self._pre_run(dispatcher)
+
+        if managed_mode or command.needs_project(dispatcher.parsed_args()):
+            self.services.project = self.get_project(
+                platform=platform, build_for=build_for
+            )
+
+        self._configure_services(provider_name)
+
+        return_code = 1  # General error
+        if not managed_mode:
+            # command runs in the outer instance
+            craft_cli.emit.debug(f"Running {self.app.name} {command.name} on host")
+            return_code = dispatcher.run() or os.EX_OK
+        elif not self.is_managed():
+            # command runs in inner instance, but this is the outer instance
+            self.run_managed(platform, build_for)
+            return_code = os.EX_OK
+        else:
+            # command runs in inner instance
+            return_code = dispatcher.run() or 0
+
+        return return_code
+
+    def run(self) -> int:
         """Bootstrap and run the application."""
         self._setup_logging()
         self._initialize_craft_parts()
-        dispatcher = self._get_dispatcher()
+
         craft_cli.emit.debug("Preparing application...")
 
-        return_code = 1  # General error
         try:
-            command = cast(
-                commands.AppCommand,
-                dispatcher.load_command(self.app_config),
-            )
-            parsed_args = dispatcher.parsed_args()
-            platform = self.get_arg_or_config(parsed_args, "platform")
-            build_for = self.get_arg_or_config(parsed_args, "build_for")
-
-            run_managed = command.run_managed(dispatcher.parsed_args())
-            is_managed = self.is_managed()
-
-            # Some commands (e.g. remote build) can allow multiple platforms
-            # or build-fors, comma-separated. In these cases, we create the
-            # project using the first defined platform.
-            if platform and "," in platform:
-                platform = platform.split(",", maxsplit=1)[0]
-            if build_for and "," in build_for:
-                build_for = build_for.split(",", maxsplit=1)[0]
-
-            provider_name = command.provider_name(dispatcher.parsed_args())
-
-            # TODO: Move pro operations out to new service for managing Ubuntu Pro
-            # A ProServices instance will only be available for lifecycle commands,
-            # which may consume pro packages,
-            self._pro_services = getattr(dispatcher.parsed_args(), "pro", None)
-            # Check that pro services are correctly configured if available
-            self._check_pro_requirement(self._pro_services, run_managed, is_managed)
-
-            if run_managed or command.needs_project(dispatcher.parsed_args()):
-                self.services.project = self.get_project(
-                    platform=platform, build_for=build_for
-                )
-
-            craft_cli.emit.debug(
-                f"Build plan: platform={platform}, build_for={build_for}"
-            )
-            self._pre_run(dispatcher)
-
-            self._configure_services(provider_name)
-
-            if not run_managed:
-                # command runs in the outer instance
-                craft_cli.emit.debug(f"Running {self.app.name} {command.name} on host")
-                return_code = dispatcher.run() or os.EX_OK
-            elif not is_managed:
-                # command runs in inner instance, but this is the outer instance
-                self.run_managed(platform, build_for)
-                return_code = os.EX_OK
-            else:
-                # command runs in inner instance
-                return_code = dispatcher.run() or 0
+            return_code = self._run_inner()
         except craft_cli.ArgumentParsingError as err:
             print(err, file=sys.stderr)  # to stderr, as argparse normally does
             craft_cli.emit.ended_ok()
