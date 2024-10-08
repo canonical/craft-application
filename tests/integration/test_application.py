460f182a1f2118a515e08e2f5844140af9f933e5
Matt Culler <matt.culler@canonical.com>
2024-10-08 11:41:42 -0400

    chore: autoformat

--- tests/integration/test_application.py
+++ tests/integration/test_application.py
@@ -13,7 +13,6 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Integration tests for the Application."""
 import argparse
-import os
 import pathlib
 import shutil
 import textwrap
