498a9fa545593b19d4128afed4468549c0173522
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-11-12 18:23:15 -0300

    feat(testing): process simplified spread.yaml
    Take a simplified version of spread.yaml supplied by the user, and
    generate the full-fledged project file to be consumed by spread.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_application/models/__init__.py
+++ craft_application/models/__init__.py
@@ -37,6 +37,11 @@ from craft_application.models.project import (
     Platform,
     Project,
 )
+from craft_application.models.spread import (
+    SimpleSpreadYaml,
+    SpreadBackend,
+    SpreadYaml,
+)
 
 
 __all__ = [
@@ -52,6 +57,9 @@ __all__ = [
     "BuildPlanner",
     "ProjectName",
     "ProjectTitle",
+    "SimpleSpreadYaml",
+    "SpreadBackend",
+    "SpreadYaml",
     "SummaryStr",
     "UniqueStrList",
     "VersionStr",
---
+++ craft_application/services/testing.py
@@ -0,0 +1,62 @@
+#  This file is part of craft-application.
+#
+#  Copyright 2024 Canonical Ltd.
+#
+#  This program is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License version 3, as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+#  SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Service for testing a project."""
+
+import pathlib
+
+from craft_cli import emit
+
+from craft_application import models, util
+
+from . import base
+
+
+class TestingService(base.AppService):
+    """Service class for testing a project."""
+
+    def process_spread_yaml(self) -> None:
+        """Process the spread configuration file.
+
+        :param project_dir: The directory to initialise the project in.
+        :param project_name: The name of the project.
+        """
+        emit.debug("Processing spread.yaml.")
+
+        craft_backend = self._get_backend()
+
+        with pathlib.Path("spread.yaml").open() as file:
+            data = util.safe_yaml_load(file)
+
+        simple = models.SimpleSpreadYaml.unmarshal(data)
+
+        spread_yaml = models.SpreadYaml.from_simple(
+            simple,
+            craft_backend=craft_backend,
+        )
+
+        spread_yaml.to_yaml_file(pathlib.Path("spread/spread.yaml"))
+
+    def _get_backend(self) -> models.SpreadBackend:
+        return models.SpreadBackend(
+            type="adhoc",
+            allocate="ADDRESS $(.extension allocate lxd-vm)",
+            discard=".extension discard lxd-vm",
+            prepare=".extension backend-prepare lxd-vm",
+            restore=".extension backend-restore lxd-vm",
+            prepare_each=".extension backend-prepare-each lxd-vm",
+            restore_each=".extension backend-restore-each lxd-vm",
+        )
