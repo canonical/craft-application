3d07b08d07229300e5d7ea92b92ef2490917e610
Alex Lowe <alex.lowe@canonical.com>
2025-01-31 19:55:36 -0500

    style: autoformat

--- craft_application/launchpad/models/distro.py
+++ craft_application/launchpad/models/distro.py
@@ -14,6 +14,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Launchpad distro_series."""
+
 import enum
 
 # This file relies heavily on dynamic features from launchpadlib that cause pyright
