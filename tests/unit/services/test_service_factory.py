0a3a25a2cf16a77b9ac64f98ef4134c670c50a62
Alex Lowe <alex.lowe@canonical.com>
2025-03-06 13:02:13 -0500

    fix: tests

--- tests/unit/services/test_service_factory.py
+++ tests/unit/services/test_service_factory.py
@@ -320,6 +320,7 @@ def test_mandatory_adoptable_field(
         LifecycleClass=fake_lifecycle_service_class,
     )
     factory.update_kwargs("project", project_dir=fake_project_file.parent)
+    factory.get("project").configure(platform=None, build_for=None)
     factory.get("project").set(fake_project)  # type: ignore[reportAttributeAccessIssue]
 
     factory.get("lifecycle")
