ee5e2f575b11195f3bd5964d0e8454a1ce6e2ef2
Alex Lowe <alex.lowe@canonical.com>
2025-03-25 15:37:43 -0400

    fix: copyright dates

--- craft_application/models/spread.py
+++ craft_application/models/spread.py
@@ -1,6 +1,6 @@
 # This file is part of craft-application.
 #
-# Copyright 2024 Canonical Ltd.
+# Copyright 2024-2025 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License version 3, as
--- craft_application/services/__init__.py
+++ craft_application/services/__init__.py
@@ -1,6 +1,6 @@
 #  This file is part of craft-application.
 #
-#  Copyright 2023 Canonical Ltd.
+#  Copyright 2023-2025 Canonical Ltd.
 #
 #  This program is free software: you can redistribute it and/or modify it
 #  under the terms of the GNU Lesser General Public License version 3, as
