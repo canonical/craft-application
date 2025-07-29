0493072630228a2568257155dbc75fae4ceade6f
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2025-07-29 18:09:54 -0300

    chore: add comment about implicit backend type
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_application/models/spread.py
+++ craft_application/models/spread.py
@@ -255,6 +255,7 @@ class SpreadYaml(SpreadBaseModel):
     ) -> dict[str, SpreadBackend]:
         backends: dict[str, SpreadBackend] = {}
         for name, backend in simple.items():
+            # Spread assumes the backend name as the type when it's not explicitly declared.
             if name == "craft" and (not backend.type or backend.type == "craft"):
                 craft_backend.systems = SpreadBackend.systems_from_craft(
                     backend.systems
