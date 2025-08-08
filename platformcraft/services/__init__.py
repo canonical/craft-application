224a9f5fd5a01bf61c19176dc2c53514bf196375
Alex Lowe <alex.lowe@canonical.com>
2025-08-08 16:28:35 -0400

    feat(WIP): platformcraft
    This creates an example platformcraft that implements fancy platforms.
    
    It doesn't contain spread tests, but it does contain a sample
    platformcraft.yaml file.

---
+++ platformcraft/services/__init__.py
@@ -0,0 +1,34 @@
+# This file is part of craft_application.
+#
+# Copyright 2025 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Services for platformcraft."""
+
+import craft_application
+
+
+def register_services() -> None:
+    """Register Partitioncraft's services.
+
+    This registers with the ServiceFactory all the services that platformcraft
+    adds or overrides.
+    """
+    craft_application.ServiceFactory.register(
+        "package", "PackageService", module="platformcraft.services.package"
+    )
+    craft_application.ServiceFactory.register(
+        "project",
+        "ProjectService",
+        module="platformcraft.services.project",
+    )
