4ddc2249641b4176fbea7ef830e179f6c5684f85
Alex Lowe <alex.lowe@canonical.com>
2025-10-17 15:46:15 -0400

    fix: hrmf
    Signed-off-by: Alex Lowe <alex.lowe@canonical.com>

--- partitioncraft/cli.py
+++ partitioncraft/cli.py
@@ -38,6 +38,7 @@ def create_app() -> craft_application.Application:
     services.get_class("provider").register_snap(
         "core24",
         Snap(name="core24", channel=None),
+    )
     services.get_class("provider").register_snap(
         "python-runtime-core24-312",
         Snap(name="python-runtime-core24-312", channel=None),
